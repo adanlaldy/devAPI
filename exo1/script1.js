2f8cd4ff5d835834336205730dc2512331f710e4
nada <adan.laldymaquiha@ynov.com>
2025-03-31 15:13:05 +0200

    feat(dev): exo 2

--- exo1/script1.js
+++ exo1/script1.js
@@ -1,9 +1,14 @@
 import fs from 'node:fs';
 
-try {
-    console.time();
-    fs.readFileSync('cheatsheet.md', 'utf8');
-    console.timeEnd();
-} catch (err) {
-    console.error(err);
+function script1() {
+    try {
+        console.log('synchrone')
+        console.time();
+        fs.readFileSync('cheatsheet.md', 'utf8');
+        console.timeEnd();
+    } catch (err) {
+        console.error(err);
+    }
 }
+
+export default script1;
---
+++ exo2/script1.js
@@ -0,0 +1,9 @@
+import script1 from "../exo1/script1.js";
+import script2 from "../exo1/script2.js";
+
+function combinedScripts() {
+    script1();
+    script2();
+}
+
+export default combinedScripts;
